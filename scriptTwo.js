b61ff16fddaf00c14114432332bdf46ecd759e66
Devin Archer <devin.archer@live.ca>
2022-10-15 18:16:37 -0600

    mostly completed. Need to figure out how to reset it without a complete refresh of the page, which then resets the counter.

--- scriptTwo.js
+++ scriptTwo.js
@@ -4,27 +4,25 @@ const playerSelection = prompt("Input Rock, Paper, or Scissors")
 const computerSelection = getComputerChoice()
 
 function getComputerChoice(min,max) {
-    //randomly return R, P or S.
-    //Returns a random integer between 1 and 3
-    //1 = rock
-    //2 = paper
-    //3 = scissors
+    // Generates a number 1, 2 or 3.
     min = Math.ceil(1)
     max = Math.floor(3)
     return Math.floor(Math.random() * (max - min +1) + min)
 }
 function pcSymbol(number){
+    // prints if pc chose rock paper or scissors
     if (number === 1){
         console.log("computer chose rock")
     } else if (number === 2) {
-        console.log("computer chose Paper")
+        console.log("computer chose paper")
     } else if (number === 3) {
-        console.log("computer chose Scissors")
+        console.log("computer chose scissors")
     } else {
     }
 }
 function playRound(player , computer){
-    let choice = player.toLowerCase()
+    // Game logic
+    let choice = player.toLowerCase();
     if (choice === "rock") {
         console.log("you selected rock")
         if (computer === 1) {
@@ -68,20 +66,22 @@ function playRound(player , computer){
     }
 }
 function game(){
-    playRound(playerSelection, computerSelection)
-    //console.log(playRound(playerSelection, computerSelection))
-    for (let i = 0; i <5; i++){
-        //this adds to user points
-        //if the return of playRound is "u"
-        
+    // is suppose to add 1 per win, don't know how to reset it.
+    switch(playRound(playerSelection, computerSelection)) {
+        case "u":
+            for (let i = 0; i < 5; i++) {
+                console.log(i)
+            break;
+            }
+        case "p":
+            for (let u = 0; u < 5; u++) {
+                console.log(u)
+            break;
+            }
     }
-
-
 }
 
-//console.log(choice)
 pcSymbol(computerSelection)
 game()
-// if pc wins, add 1 to their tally
-// if user wins add 1 to their tally
-// to a maximum of 5
+
+
