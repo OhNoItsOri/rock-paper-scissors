254461938b53b357bd348afbfc47726d6716d654
Devin Archer <devin.archer@live.ca>
2022-10-19 00:18:30 -0600

    rewrote the javascript portion to make it work...

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,87 +1,78 @@
+const options = ["rock", "paper", "scissors"];
 
-
-const playerSelection = prompt("Input Rock, Paper, or Scissors")
-const computerSelection = getComputerChoice()
-
-function getComputerChoice(min,max) {
-    // Generates a number 1, 2 or 3.
-    min = Math.ceil(1)
-    max = Math.floor(3)
-    return Math.floor(Math.random() * (max - min +1) + min)
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
-function pcSymbol(number){
-    // prints if pc chose rock paper or scissors
-    if (number === 1){
-        console.log("computer chose rock")
-    } else if (number === 2) {
-        console.log("computer chose paper")
-    } else if (number === 3) {
-        console.log("computer chose scissors")
-    } else {
+
+function winChecker(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
     }
 }
-function playRound(player , computer){
-    // Game logic
-    let choice = player.toLowerCase();
-    if (choice === "rock") {
-        console.log("you selected rock")
-        if (computer === 1) {
-            console.log("tie game!")
-            return "t"
-        } else if (computer === 2) {
-            console.log("Computer wins! Better Luck Next Time")
-            return "p"
-        } else if (computer === 3) {
-            console.log("You win! Congrats")
-            return "u"
-        } else ;{
+
+function playRound(playerSelection, computerSelection){
+    const result = winChecker(computerSelection, playerSelection);
+    if (result == "Tie"){
+        return "it's a tie!"
+    }
+    else if (result == "Player"){
+        return "You win!"
+    }
+    else{
+        return "you lose!"
+    }
+
+}
+
+function getPlayerChoice(){
+    let validatedInput = false;
+    while (validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if (choice == null){
+            continue;
         }
-    } else if (choice === "paper") {
-        console.log("you selected paper");
-        if (computer === 1) {
-            console.log("You win! Congrats!")
-            return "u"
-        } else if (computer === 2) {
-            console.log("Tie game!")
-            return "t"
-        } else if (computer === 3) {
-            console.log("Computer wins. Better luck next time!")
-            return "p"
-        } else ;{
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
         }
-    } else if (choice === "scissors") {
-        console.log("you selected scissors");
-        if (computer === 1) {
-            console.log("computer wins. better luck next time!")
-            return "p"
-        } else if (computer === 2) {
-            console.log("you win! congrats")
-            return "u"
-        } else if (computer === 3) {
-            console.log("tie game!")
-            return "t"
-        } else ;{
-        }
-    } else ;{
     }
 }
+
 function game(){
-    // is suppose to add 1 per win, don't know how to reset it.
-    switch(playRound(playerSelection, computerSelection)) {
-        case "u":
-            for (let i = 0; i < 5; i++) {
-                console.log(i)
-            break;
-            }
-        case "p":
-            for (let u = 0; u < 5; u++) {
-                console.log(u)
-            break;
-            }
+    const scorePlayer = 0;
+    const scoreComputer = 0;
+    console.log("so it begins")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if (winChecker(playerSelection, computerSelection) == "Player"){
+            scorePlayer++;
+        }
+        else if(winChecker(playerSelection, computerSelection) == "Computer"){
+            scoreComputer++;
+        }
     }
+    
+    console.log("game over")
+    if (scorePlayer > scoreComputer){
+        console.log("player was the winner");
+    } else if ( scorePlayer < scoreComputer){
+        console.log("computer was the winner");
+    }
+
 }
 
-pcSymbol(computerSelection)
 game()
-
-
