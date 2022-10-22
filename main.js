05d775e7f627b281da8d7139a31fca33e05bf405
Devin Archer <devin.archer@live.ca>
2022-10-22 02:04:44 -0600

    rethinking everyting

--- main.js
+++ main.js
@@ -3,17 +3,26 @@
     a warcrime.
 */
 
-/* Below this is the game logic. Set up for the site is above*/
-
+const button = document.querySelectorAll("button");
 const options = ["rock", "paper", "scissors"];
+
+button.forEach((button) => {
+    button.addEventListener("click", () => {
+        result(button.id, getComputerChoice());
+    });
+});
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
-function winChecker(playerSelection, computerSelection){
+// playerSelection is inherrited from button interface and the function call
+// ComputerSelection is inherrited from the function getComputerChoice()
+
+var result = function winChecker(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
+        console.log("Tie game");
         return "Tie";
     }
     else if(
@@ -21,59 +30,31 @@ function winChecker(playerSelection, computerSelection){
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")
     ){
+        console.log("Player Wins" + " Player chose " + playerSelection + " PC chose " + computerSelection );
         return "Player";
     }
     else {
+        console.log("Computer Wins" + " Player chose " + playerSelection + " PC chose " + computerSelection );
         return "Computer";
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    const result = winChecker(computerSelection, playerSelection);
-    if (result == "Tie"){
-        return "it's a tie!"
-    }
-    else if (result == "Player"){
-        return "You win!"
-    }
-    else{
-        return "you lose!"
-    }
-}
-
-function getPlayerChoice(){
-    const button = document.querySelectorAll("button");
-    button.forEach((button) => {
-        button.addEventListener("click", () => {
-            getPlayerClick = button.id;
-            console.log("your choice is " + getPlayerClick);
-        });
-    });
-}
+// Not sure where to implement the score thing.
 
 function game(){
     var scorePlayer = 0;
     var scoreComputer = 0;
-    console.log("so it begins")
     for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice(); // GET INPUT FROM BUTTONS
-        console.log(playerSelection);
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if (winChecker(playerSelection, computerSelection) == "Player"){
+        if (result == "Player"){
             scorePlayer++;
         }
-        else if(winChecker(playerSelection, computerSelection) == "Computer"){
+        else if(result == "Computer"){
             scoreComputer++;
         }
     }
-    
-    console.log("game over")
     if (scorePlayer > scoreComputer){
-        console.log("player was the winner" + " pc score =" + scoreComputer);
+        console.log("player was the winner" + " player score = " + scorePlayer);
     } else if ( scorePlayer < scoreComputer){
-        console.log("computer was the winner" + " player score " + scorePlayer);
+        console.log("computer was the winner" + " computer score = " + scoreComputer);
     }
 }
-
-game()
